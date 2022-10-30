951f0667272df22d2de96693b0cb9b5904cc45a0
personalcurso <cursocurso394@gmail.com>
2022-10-30 17:44:33 -0500

    se modifico funcio_rama_angel

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,4 +2,4 @@ def funcion_principal():
     print("commit del archivo ejercicio1 en master")
 
 def funcion_rama_angel():
-    print("esto se ve solo en mi rama")
+    print("esto se ve solo en mi ramam angel")
